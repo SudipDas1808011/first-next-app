edb7e9c25318b248216f6aebc728b637197c4113
SudipDas1808011 <sudipdas11804144@gmail.com>
2025-09-16 17:06:12 +0600

    nested layout added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,23 @@ export default function Layout({children}:{children: React.ReactNode}){
     return (
     <html>
         <body>
+            <header
+                style={{
+                    backgroundColor: "lightblue",
+                    padding:'1rem'
+                }}
+                >
+                    <p>Header</p>
+                </header>
             {children}
+            <footer
+                style={{
+                    backgroundColor:"lightgreen",
+                    padding:"1rem"
+                }}
+                >
+                    <p> footer </p>
+                </footer>
         </body>
     </html>
 )
