949b95400f1cce10ad727efda08c5a0889792d06
SudipDas1808011 <sudipdas11804144@gmail.com>
2025-09-16 13:17:20 +0600

    added custom not found dynamically

--- src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -1,10 +1,14 @@
 "use client";
 import { usePathname } from "next/navigation";
+import path from "path";
 
 export default function NotFound(){
+    const pathname = usePathname();
+    const productId = pathname.split("/")[2];
+    const reviewId = pathname.split("/")[4];
     return(
         <div>
-            <h2> Review not found </h2>
+            <h2> Review {reviewId} not found for product {productId} </h2>
             
         </div>
     );
